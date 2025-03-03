803a232b73cd578a4e87fb079b6d88e583b08138
Rafael468 <abadginerrafa@gmail.com>
2025-03-03 18:38:08 +0000

    Second commit

--- src/app.js
+++ src/app.js
@@ -9,9 +9,7 @@ window.onload = function() {
   //write your code here
   const symbolArray=["♦", "♥", "♠", "♣"];
   const numberArray=["A","2","3","4","5","6","7","8","9","J","Q","K"];
-  /*next steps:
-  -use penis function for each onload(or reload if you're fancy)
-  */
+
  function getRandomIndex(array){
   let index = Math.floor(Math.random() * array.length)
   return array[index];
